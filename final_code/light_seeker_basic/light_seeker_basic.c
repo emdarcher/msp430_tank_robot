4732b473232ad5bb738244abe4568a637836b6a7
emdarcher <emd519@yahoo.com>
2014-06-11 19:06:17 -0700

    final code works and is finished now (as far as i know)

--- final_code/light_seeker_basic/light_seeker_basic.c
+++ final_code/light_seeker_basic/light_seeker_basic.c
@@ -1,5 +1,7 @@
 //main code
-//test code for pwm, LDRs, and motors on msp430 tank robot
+//compiled with mspgcc
+
+//final code for pwm, LDRs, and motors on msp430 tank robot
 //the output from the two LDR is compared to find which one is greater and 
 //the difference between their values.
 //The difference is then applied to a difference in pwms from a midpoint
@@ -17,18 +19,20 @@
 
 //	Preprocessor Definitions
 
-//motor control pins
-#define MOTOR_R_A BIT7
+//motor control pins, go to L293D motor driver
+#define MOTOR_R_A BIT7      //
 #define MOTOR_R_B BIT6		//P1.6 has TA0.1 output
 
-#define MOTOR_L_A BIT3		//note changes here!
-#define MOTOR_L_B BIT4		//now on P1.4 for TA0.2 output
+#define MOTOR_L_A BIT3		//
+#define MOTOR_L_B BIT4		// on P1.4 for TA0.2 output
 
-#define MOTOR_L_AB ( MOTOR_L_A | MOTOR_L_B )
+#define MOTOR_L_AB ( MOTOR_L_A | MOTOR_L_B )  //both of the pins for the motors
 #define MOTOR_R_AB ( MOTOR_R_A | MOTOR_R_B )
 
+//has all motor pwm pins
 #define MOTOR_LR_PWM ( MOTOR_L_B | MOTOR_R_B )
 
+//has all motor pins
 #define ALL_MOTORS ( MOTOR_L_A | MOTOR_L_B | MOTOR_R_A | MOTOR_R_B )
 
 //analog input pins
@@ -44,8 +48,6 @@
 #define FULL_PWM PWM_TOP
 #define MID_PWM (FULL_PWM/2)
 
-
-
 //	Global Variables
 
 unsigned int analog_vals[2]={0,0};
@@ -53,26 +55,9 @@ unsigned int a0_val = 0;
 unsigned int a1_val = 0;
 unsigned int pwm_vals_lr[2] = {0,0};
 
-
 const unsigned char motor_a_pins[] = {MOTOR_L_A, MOTOR_R_A};
-//const unsigned char motor_ab_pins[] = {MOTOR_L_AB, MOTOR_R_AB};
-
-//int i; //for "for" loops
 
 //	Function Prototypes/Definitions
-void go_forwards(int spd);
-void go_reverse(int spd);
-void spin_right(int spd);
-void spin_left(int spd);
-void skid_right(int spd);
-void skid_left(int spd);
-void stop_motors(void);
-
-void motor_l_fwd(int spd);
-void motor_l_rev(int spd);
-void motor_r_fwd(int spd);
-void motor_r_rev(int spd);
-
 void set_motor_speed(unsigned char lr, int speed);
 void set_motor_speeds(int speed_left, int speed_right);
 
@@ -87,6 +72,7 @@ int analog_to_pwm(unsigned int analog);
 void diff_to_pwms(unsigned char side, unsigned int difference);
 void process_vals(unsigned int val1, unsigned int val2 );
 
+//a delay function (unused here)
 void bad_delay_ms(unsigned int ms){
 	int j;
 	for (j=0;j<ms;j++){
@@ -95,31 +81,30 @@ void bad_delay_ms(unsigned int ms){
 	}
 }
 
+
+//main program
 void main(void) {
 	WDTCTL = WDTPW + WDTHOLD; //disable watchdog
 	
 	//initialization stuff here
 		//remember to enable any pull-up/downs!
 	
-	P1OUT = 0;
-	P1DIR |= ALL_MOTORS;
-	P1SEL |= MOTOR_LR_PWM;
+	P1OUT = 0; //turn off all outputs
+	P1DIR |= ALL_MOTORS; //select motor pins as outputs
+	P1SEL |= MOTOR_LR_PWM;  //select the motor pwm pins
 	P1SEL2 |= MOTOR_L_B; //P1SEL2 needed as well for TA0.2
 	
 	BCSCTL1 = CALBC1_1MHZ;          // Running at 1 MHz
     DCOCTL = CALDCO_1MHZ;
 
-	TA_init();
+	TA_init();//initialize Timer_A for PWM
     
-    ADC_init();
+    ADC_init();//initialize the internal ADC10 for reading the analog values
+                //of the LDRs/Photoresistors
     
-    //_BIS_SR(LPM0_bits + GIE);       // Enter LPM0 and enable interrupts
-	
-	
 	//infinite loop
 	for(;;) {
-		
-		ADC_read_vals(); //read ADC input values
+		ADC_read_vals(); //read ADC input values and go into LPM0 while reading
 		process_vals(a0_val,a1_val); //process values and set motor pwms accordingly
 	}
 	
@@ -127,22 +112,20 @@ void main(void) {
 
 //	Functions
 void process_vals(unsigned int val1, unsigned int val2 ){
-     //processes vals to determine left or right brighter and diff.
+     //processes vals to determine if left or right is brighter and the diff.
     //val1 will be the left and val2 right in this case
     
-    //unsigned int diff_vals;
-    
-    val1 = analog_to_pwm(val1);
+    val1 = analog_to_pwm(val1); //convert to value in pwm range
     val2 = analog_to_pwm(val2);
     
     if(val1 == val2) {
         set_motor_speeds(FULL_PWM,FULL_PWM);
     }
-    else if(val1 > val2){
-        diff_to_pwms(0x00,(val1-val2));
+    else if(val1 > val2){//if val1 is greater, send direction left and diff
+        diff_to_pwms(0,(val1-val2));
     }
-    else if(val1 < val2){
-        diff_to_pwms(0x01,(val2-val1));
+    else if(val1 < val2){//if val2 is greater, send direction right and diff
+        diff_to_pwms(1,(val2-val1));
     }
             
 }
@@ -151,20 +134,24 @@ void process_vals(unsigned int val1, unsigned int val2 ){
 void diff_to_pwms(unsigned char side, unsigned int difference){
     //side can be 0 or 1, left or right, respectively
     
-    unsigned int half_diff = (difference/2);
-    //pwm_vals_lr[side] = MID_PWM + half_diff;
-    //pwm_vals_lr[!(side)] = MID_PWM - half_diff;
+    unsigned int half_diff = (difference/2); //find half of difference
     
-    //switched for opposite sides b/c it is turning to light with motors
+    //switched for opposite sides b/c it is turning towards light with motors
+    
+    //add or subtract half of difference to middle pwm value on each motor
+    //to create the difference between the pwms the same as full difference,
+    //just relative to the middle.
     pwm_vals_lr[side] = MID_PWM - half_diff;
     pwm_vals_lr[!(side)] = MID_PWM + half_diff;
-          
+    
+    //send the resulting values to the motors
     set_motor_speeds(pwm_vals_lr[0],pwm_vals_lr[1]);
     
 }
 
 void set_motor_speeds(int speed_left, int speed_right){
-	
+	//set the speeds for both motors in this function
+    //which calls the set_motor_speed function for each motor
 	set_motor_speed(0, speed_left);
 	set_motor_speed(1, speed_right);
 	
@@ -188,89 +175,23 @@ if(lr < 2){ //just check to make sure lr value is valid 0 or 1
 	
 	switch (lr){ //sets which PWM pin to set
 		case 0:
-		TACCR2 = outspeed;
+		TACCR2 = outspeed; //set left motor pwm
 			break;
 		case 1:
-		TACCR1 = outspeed;
+		TACCR1 = outspeed; //set right motor pwm
 			break;
 	}
 }
 
 }
 
-void stop_motors(void){
-	//P1OUT &= ~(MOTOR_L_A | MOTOR_L_B | MOTOR_R_A | MOTOR_R_B);
-	//P1OUT &= ~ALL_MOTORS;
-	set_motor_speeds(0,0);
-}
-
-void motor_l_fwd(int spd){
-	//P1OUT |= MOTOR_L_A;
-	//P1OUT &= ~MOTOR_L_B;
-	
-	set_motor_speed(0,spd);
-	
-}
-void motor_l_rev(int spd){
-	//P1OUT |= MOTOR_L_B;
-	//P1OUT &= ~MOTOR_L_A;
-	spd *= -1;//make negative
-	set_motor_speed(0,spd);
-}
-void motor_r_fwd(int spd){
-	//P1OUT |= MOTOR_R_A;
-	//P1OUT &= ~MOTOR_R_B;
-	set_motor_speed(1,spd);
-}
-void motor_r_rev(int spd){
-	//P1OUT |= MOTOR_R_B;
-	//P1OUT &= ~MOTOR_R_A;
-	spd *= -1;//make negative
-	set_motor_speed(1,spd);
-}
-
-void go_forwards(int spd){
-	//P1OUT |= (MOTOR_L_A | MOTOR_R_A);
-	//P1OUT &= ~(MOTOR_L_B | MOTOR_R_B);
-	
-	set_motor_speeds(spd,spd);
-}
-void go_reverse(int spd){
-	//P1OUT |= (MOTOR_L_B | MOTOR_R_B);
-	//P1OUT &= ~(MOTOR_L_A | MOTOR_R_A);
-	spd *= -1;//make negative
-	set_motor_speeds(spd,spd);
-}
-
-void skid_left(int spd){
-	//P1OUT |= (MOTOR_R_A);
-	//P1OUT &= ~(MOTOR_L_A | MOTOR_L_B | MOTOR_R_B);
-	set_motor_speeds(0,spd);
-}
-void skid_right(int spd){
-	//P1OUT |= (MOTOR_L_A);
-	//P1OUT &= ~(MOTOR_L_B | MOTOR_R_A | MOTOR_R_B);
-	set_motor_speeds(spd,0);
-}
-void spin_left(int spd){
-	//P1OUT |= (MOTOR_L_B | MOTOR_R_A);
-	//P1OUT &= ~(MOTOR_L_A | MOTOR_R_B);
-	set_motor_speeds((-1*spd),spd);
-}
-void spin_right(int spd){
-	//P1OUT |= (MOTOR_L_A | MOTOR_R_B);
-	//P1OUT &= ~(MOTOR_L_B | MOTOR_R_A);
-	set_motor_speeds(spd,(-1*spd));
-}
 
 int analog_to_pwm(unsigned int analog){
 	//to convert the analog 10 bit 0-1023
     //to 0-1000, but just by subtracting 32 if over 1000 or PWM_TOP.
     //not directly accurate, but close enough in this case and less math.
 	if( analog > PWM_TOP){
-		
 		return (analog - 23);
-		
 	} else {
 		return analog;
 	}
@@ -286,7 +207,7 @@ void TA_init(void) {
 	
 	//stuff for TA0.2
 	TACCR2 = 0; //0.0%
-	TACCTL2 = OUTMOD_7;
+	TACCTL2 = OUTMOD_7; //same as above but for TACCR2
 	
 }
 
@@ -294,7 +215,7 @@ void ADC_init(void){
 	
 	ADC10CTL1 = INCH_1 + CONSEQ_1;
 	// ADC10CTL1 control register 1, channel 1 highest conversion channel
-    //make sure INCH_X val is correct!!!!
+    // make sure INCH_X val is correct!!!!
 	// CONSEQ_1 conversion seq mode 3 repeat sequence of channels
 	ADC10CTL0 = ADC10SHT_1 + MSC + ADC10ON + ADC10IE + SREF_0;
 	// ADC10CTL0 control Register 0
@@ -306,22 +227,22 @@ void ADC_init(void){
 	ADC10DTC1 = 0x02;
 	// Data Transfer Control register, defines the number of transfers in each block, 2 in this case
 	ADC10AE0 |= (A0 | A1);
-	// ADC10 analog enable bits PP1.1 and P1.0 
+	// ADC10 analog enable bits P1.1 and P1.0 
 }
 
 void ADC_read_vals(void){
 	
 	ADC10CTL0 &= ~ENC; //disable conversion
 	while (ADC10CTL1 & BUSY);               // Wait if ADC10 core is active
-	ADC10SA = (unsigned int)&analog_vals[0];			// Copies data in ADC10SA to unsigned int adc array
+	ADC10SA = (unsigned int)&analog_vals[0];// Copies data in ADC10SA to unsigned int analog_vals array
     ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-	//original way, but a little less nice lookin'
-	//__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
 	
+    
 	_BIS_SR(LPM0_bits + GIE);       // Enter LPM0 and enable interrupts
+                                    // the ADC10_ISR will exit this from LPM0.
 	
-	a0_val = analog_vals[1];
-	a1_val = analog_vals[0];
+	a0_val = analog_vals[1]; //copy the values from analog_vals[]
+	a1_val = analog_vals[0]; //into variables to store each value
 	
 }
 
@@ -330,10 +251,8 @@ void ADC_read_vals(void){
 __attribute__((interrupt(ADC10_VECTOR)))
 void ADC10_ISR(void){
 	
-	//__bic_SR_register_on_exit(CPUOFF);        // Clear CPUOFF bit from 0(SR)
-	//
-	//
-	_BIC_SR_IRQ(LPM0_bits); //following convention
+	_BIC_SR_IRQ(LPM0_bits); //following convention and clearing the LPM0_bits
+                            //to leave LPM0 and continue ADC_read_vals()
 	
 	//simpler way:
 	//LPM0_EXIT;
